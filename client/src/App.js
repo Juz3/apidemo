a002a18abec3b5634886d5bf65574bc6a214ddb9
Juz3 <jussi.koivumaki@outlook.com>
2022-05-13 00:44:41 +0300

    clean cra template

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="Main-container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Coding assignment</p>
+        <p>Demo project with React client and node.js REST API</p>
       </header>
     </div>
   );
--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-// server.js
+/* Demo App REST API */
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Demo app REST API listening on port ${port}`);
+});
