6fbcd071492bf01db80d4588f597dcaa4c14e82b
Juz3 <jussi.koivumaki@outlook.com>
2022-05-16 01:35:21 +0300

    cleanup

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-/* Demo App REST API */
 const express = require("express");
 
 const checkPrime = require("./api/checkPrime");
@@ -6,8 +5,6 @@ const sumAndCheck = require("./api/sumAndCheck");
 
 const app = express();
 const port = 5000;
-
-app.use(express.json({ extended: true }));
 
 app.use("/api/checkprime", checkPrime);
 app.use("/api/sumandcheck", sumAndCheck);
